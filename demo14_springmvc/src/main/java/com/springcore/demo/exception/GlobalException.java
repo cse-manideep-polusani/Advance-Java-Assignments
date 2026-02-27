0704fa5cdabfe155bf4f9f04eef4a63d14a8cea9
cse-manideep-polusani <cse.manideep.polusani@gmail.com>
2026-02-27 15:13:57 +0530

    Spring

---
+++ demo14_springmvc/src/main/java/com/springcore/demo/exception/GlobalException.java
@@ -0,0 +1,5 @@
+package com.springcore.demo.exception;
+
+public class GlobalException {
+
+}
---
+++ demo16_springrest/src/main/java/com/springmvc/demo/Demo16SpringrestApplication.java
@@ -0,0 +1,13 @@
+package com.springmvc.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo16SpringrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo16SpringrestApplication.class, args);
+	}
+
+}
